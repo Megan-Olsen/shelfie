9ab5e8490972a10b3b0fd18a4d8e01640ef7bbb7
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-04 16:32:02 -0600

    stuck on props, again

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Form from "./Components/Form/Form";
 
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      inventory: [
+        {name: "the great stuff", imgurl: "tacossldkjfiong", price: 99.99}, 
+        {name: "see the things", umgurl: "what!", price: 666}
+      ]
+    }
+  }
+    
   render(){
     return (
       <div>
