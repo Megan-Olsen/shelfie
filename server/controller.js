777b955f88bf3cf3b1d2a545d76bce712698588c
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-06 16:08:25 -0600

    did mount

--- server/controller.js
+++ server/controller.js
@@ -9,13 +9,3 @@ module.exports = {
         })
   },
 };
-// getAll: (req, res, next ) => {
-//     const dbInstance = req.app.get('db');
-
-//     dbInstance.read_products()
-//         .then( products => res.status(200).send( products ) )
-//         .catch( err => {
-//             res.status(500).send({errorMessage: "Its broken you silly goose."});
-//             console.log(err)
-//      } );
-// },
