3cc0edd80dd0264b7870bdace9ee4f412f754c5a
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-06 15:47:10 -0600

    finally got database working

--- server/controller.js
+++ server/controller.js
@@ -1,5 +1,21 @@
 module.exports = {
   getInventory: (req, res) => {
-    res.status(200).send("its working");
+      const dbInstance = req.app.get('db');
+
+      dbInstance.get_inventory().then(inventory => res.status(200).send(inventory))
+        .catch (err => {
+            res.status(500).send({errorMessage: "this isn't working"})
+            console.log(err)
+        })
   },
 };
+// getAll: (req, res, next ) => {
+//     const dbInstance = req.app.get('db');
+
+//     dbInstance.read_products()
+//         .then( products => res.status(200).send( products ) )
+//         .catch( err => {
+//             res.status(500).send({errorMessage: "Its broken you silly goose."});
+//             console.log(err)
+//      } );
+// },
--- server/index.js
+++ server/index.js
@@ -9,17 +9,20 @@ const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive({
   connectionString: CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-});
+  ssl: { rejectUnauthorized: false }
+}).then(dbInstance => {
+    app.set('db', dbInstance);
+}).catch(err => console.log(err));
 // .then(dbInstance => {
-//     app.set('db', dbInstance);
-// }).catch(err => console.log(err));
-
+    //     app.set('db', dbInstance);
+    // }).catch(err => console.log(err));
+    
 app.use(express.json());
-
+    
 app.get("/api/inventory", ctrl.getInventory);
-// app.post("api/product", ctrl.addProduct);
-
+    // app.post("api/product", ctrl.addProduct);
+    
+    
 app.listen(SERVER_PORT, () =>
-  console.log(`Shhhh.... the Dwarves are working on port ${SERVER_PORT}`)
+    console.log(`Shhhh.... the Dwarves are working on port ${SERVER_PORT}`)
 );
