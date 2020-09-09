684bac1774b910af0841548b35d5f221bafb9ccb
Megan Olsen <megan.a.olsen63@gmail.com>
2020-09-09 08:32:18 -0600

    need to fix update and routes and wrap text

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,10 +5,12 @@ function Product(props) {
     return (
         <div className="inventory-container">
             <p className="inventory-text">{name}</p>
-            <p className="inventory-text">{`$${price}`}</p>
+            <p className="inventory-price">{`$${price}`}</p>
             <img className="inventory-img" src={image_url} alt={`This is a ${name}`} />
+            <div className="buttons">
             <button onClick={() => deleteProduct(id)}>Delete</button>
             <button onClick={() => updateProduct(props.item)}>Update</button>
+            </div>
         </div>
     )
 }
